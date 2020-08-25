6ab10869918ba16b5f52a076b4aba44b30519388
HC-bentill <henrybentil88@gmail.com>
2020-08-25 11:50:39 +0000

    Updates

---
+++ precache-manifest.7bc107003febb7264b78158488a7cfd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b32500e3d74890cb2f29934b9046be2",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "1e324285088fd4fb10cc",
+    "url": "/Netflix-clone/static/css/main.b5a5e655.chunk.css"
+  },
+  {
+    "revision": "0d6a5fd80ef216c6b98f",
+    "url": "/Netflix-clone/static/js/2.a879a97c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone/static/js/2.a879a97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e324285088fd4fb10cc",
+    "url": "/Netflix-clone/static/js/main.d25148a7.chunk.js"
+  },
+  {
+    "revision": "1f70fcb38d92599a6c08",
+    "url": "/Netflix-clone/static/js/runtime-main.d8bb3d81.js"
+  }
+]);
